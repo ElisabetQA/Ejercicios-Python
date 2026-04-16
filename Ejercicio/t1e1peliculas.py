afe672b2d52b8a8c4f23586118d1077a3f489310
Elisabet Gacía <elisabet.gar@gmail.com>
2026-04-16 13:17:03 +0100

    Ejercicio 1

---
+++ Ejercicio/t1e1peliculas.py
@@ -0,0 +1,15 @@
+#Variables
+titulo_de_la_pelicula = "Aladdin"
+director_de_la_pelicula = "Ron Clements, John Musker"
+año_de_lanzamiento = 1992
+genero = "Animación, Fantasía, Musical"
+duracion_en_minutos = 90
+tiene_premmios = False
+
+#Imprimir las variables
+print("Título de la película:", titulo_de_la_pelicula)
+print("Director de la película:", director_de_la_pelicula)
+print("Año de lanzamiento:", año_de_lanzamiento)
+print("Género:", genero)
+print("Duración en minutos:", duracion_en_minutos)
+print("¿Tiene premios?:", tiene_premmios)
