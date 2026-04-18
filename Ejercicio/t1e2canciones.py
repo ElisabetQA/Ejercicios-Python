c6a7024b347a843b44a75c42f50da00653ad270e
Elisabet Gacía <elisabet.gar@gmail.com>
2026-04-18 21:04:24 +0100

    update t2e1calculadora.py

--- Ejercicio/t1e2canciones.py
+++ Ejercicio/t1e2canciones.py
@@ -1,4 +1,4 @@
-#Obtener un valor del usuario
+#Obtener los valores del usuario de su canción favorita
 titulo_de_la_cancion = input ("¿Cuál es tu canción favorita? ")
 artista = input ("¿Cuál es su artista? ")
 album = input ("¿A qué álbum pertenece? ")
@@ -6,6 +6,7 @@ año_de_lanzamiento = input ("¿En qué año se lanzó? ")
 duracion_en_segundos = int (input ("¿Cuántos segundos dura? "))
 tiene_videoclip = input ("¿Tiene videoclip? (True o False) ")
 
+#imprimir resultados de su canción favorita
 print("----> RESUMEN DE LA CANCIÓN QUE MÁS TE GUSTA")
 print("Tu canción favorita es", titulo_de_la_cancion)
 print("El artista es", artista)
@@ -15,7 +16,7 @@ print("La canción dura", duracion_en_segundos, "segundos")
 print("¿Tiene videclip?", tiene_videoclip)
 
 
-#Obtener un valor del usuario v.2 peor
+#Obtener los valores del usuario de la cación que menos le gusta v.2peor
 peor_titulo_de_la_cancion = input ("¿Cuál es la canción que menos te gusta? ")
 peor_artista = input ("¿Cuál es su artista? ")
 peor_album = input ("¿A qué álbum pertenece? ")
@@ -23,6 +24,7 @@ peor_año_de_lanzamiento = input ("¿En qué año se lanzó? ")
 peor_duracion_en_segundos = int (input ("¿Cuántos segundos dura? "))
 peor_tiene_videoclip = input ("¿Tiene videoclip? (True o False) ")
 
+#imprimir resultados de la cación que menos le gusta v.2peor
 print("----> RESUMEN DE LA CANCIÓN QUE MENOS TE GUSTA")
 print("La canción que menos te gusta es", peor_titulo_de_la_cancion)
 print("El artista es", peor_artista)
---
+++ Ejercicio/t2e1calculadora.py
@@ -0,0 +1,15 @@
+#Pedir al usuario dos números
+numero1 = float (input ("Dime un número: "))
+numero2 = float (input ("Dime otro número: "))
+
+#Operaciones
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+#Imprimos las operaciones
+print("Suma: ", suma)
+print("Resta: ", resta)
+print("Multiplicación: ", multiplicacion)
+print("División: ", division)
