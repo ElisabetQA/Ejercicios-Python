5149c5e2c1d953295ab376bba4077d39c2e7ad02
Elisabet Gacía <elisabet.gar@gmail.com>
2026-04-18 20:44:02 +0100

    Update t1e2canciones.py

--- Ejercicio/t1e2canciones.py
+++ Ejercicio/t1e2canciones.py
@@ -6,9 +6,27 @@ año_de_lanzamiento = input ("¿En qué año se lanzó? ")
 duracion_en_segundos = int (input ("¿Cuántos segundos dura? "))
 tiene_videoclip = input ("¿Tiene videoclip? (True o False) ")
 
+print("----> RESUMEN DE LA CANCIÓN QUE MÁS TE GUSTA")
 print("Tu canción favorita es", titulo_de_la_cancion)
 print("El artista es", artista)
 print("Pertenece al álbum", album)
 print("Se lanzó en el año", año_de_lanzamiento)
 print("La canción dura", duracion_en_segundos, "segundos")
 print("¿Tiene videclip?", tiene_videoclip)
+
+
+#Obtener un valor del usuario v.2 peor
+peor_titulo_de_la_cancion = input ("¿Cuál es la canción que menos te gusta? ")
+peor_artista = input ("¿Cuál es su artista? ")
+peor_album = input ("¿A qué álbum pertenece? ")
+peor_año_de_lanzamiento = input ("¿En qué año se lanzó? ")
+peor_duracion_en_segundos = int (input ("¿Cuántos segundos dura? "))
+peor_tiene_videoclip = input ("¿Tiene videoclip? (True o False) ")
+
+print("----> RESUMEN DE LA CANCIÓN QUE MENOS TE GUSTA")
+print("La canción que menos te gusta es", peor_titulo_de_la_cancion)
+print("El artista es", peor_artista)
+print("Pertenece al álbum", peor_album)
+print("Se lanzó en el año", peor_año_de_lanzamiento)
+print("La canción dura", peor_duracion_en_segundos, "segundos")
+print("¿Tiene videclip?", peor_tiene_videoclip)
