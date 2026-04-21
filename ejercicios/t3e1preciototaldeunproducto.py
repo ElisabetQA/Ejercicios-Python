1e495a9051a9b9b2376226351fc81e0cc22be8cb
Elisabet Gacía <elisabet.gar@gmail.com>
2026-04-21 18:30:49 +0100

    updata t3e1preciototaldeunproducto.py

--- ejercicios/t3e1preciototaldeunproducto.py
+++ ejercicios/t3e1preciototaldeunproducto.py
@@ -1,18 +1,19 @@
 #Solicitud de parámetros al usuario
 nombre_del_producto = input ("¿Cuál es el nombre del producto? ")
-precio_por_unidad = float (input ("¿Cuánto es la inversión de una unidad? "))
+precio_por_unidad = float (input ("¿Cuánto es el precio de una unidad? "))
 cantidad_a_comprar = int (input ("¿Cuántos vas a comprar? "))
-descuento_en_porcentaje = float (input ("¿Qué porcentaje de decuento tiene? "))
+descuento_en_porcentaje = float (input ("¿Qué porcentaje de descuento tiene? "))
 IVA_en_porcentaje = float (input ("¿Qué porcentaje de IVA tiene? "))
 
 #Función calcular el precio total aplicando descuento e IVA
-def calcular_precio_total (precio_por_unidad, cantidad_a_comprar, descuento_en_porcentaje, IVA_en_porcentaje):
-    resultado = (precio_por_unidad * cantidad_a_comprar)
-    return resultado
+def calcular_operaciones (precio_por_unidad, cantidad_a_comprar, descuento_en_porcentaje, IVA_en_porcentaje):
+    total_producto = precio_por_unidad * cantidad_a_comprar
+    total_descuento = (total_producto - (total_producto * descuento_en_porcentaje)/100)
+    total_con_IVA = (total_descuento + (total_descuento * IVA_en_porcentaje)/100)
+    return total_con_IVA
 
-#Guardar el resultado como variable 
-resultado = calcular_precio_total (precio_por_unidad, cantidad_a_comprar, descuento_en_porcentaje, IVA_en_porcentaje)
+# Guardar el resultado en una variable
+total_compra = calcular_operaciones (precio_por_unidad, cantidad_a_comprar, descuento_en_porcentaje, IVA_en_porcentaje)
 
-
-#Imprimir resultado
-print("El total del producto es ", resultado)
+#Imprimir total compra
+print("El total de la compra es ", total_compra, "euros")
