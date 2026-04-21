b15cbf6713ad7296df6dd4a9f72ecfeb6a535769
Elisabet Gacía <elisabet.gar@gmail.com>
2026-04-21 18:48:10 +0100

    Create t3e2conversordemonedas.py

---
+++ ejercicios/t3e2conversordemonedas.py
@@ -0,0 +1,21 @@
+#Pedir al usuario una cantidad en euros
+cantidad_en_euros = float ( input ("Dime una cantidad en euros "))
+
+#Convertir la cantidad de euros en otras monedas 
+# Dólares (1 euro = 1.1 dólares) 
+# Libras (1 euro = 0.87 libras).
+
+dolares = 1.1
+libras = 0.87
+
+def convertir (cantidad_en_euros, dolares, libras):
+    total_dolares = cantidad_en_euros * dolares
+    total_libras = cantidad_en_euros * libras
+    return total_dolares, total_libras
+
+#imprimir varios resultados
+total_dolares, total_libras = convertir (cantidad_en_euros, dolares, libras)
+print ("Total en dólares", total_dolares)
+print ("Total en libras", total_libras)
+
+    
