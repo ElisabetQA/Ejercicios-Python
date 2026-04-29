e8d7fb3177acde9a549cda7860f24912c5ea3fdd
Elisabet Gacía <elisabet.gar@gmail.com>
2026-04-29 10:53:02 +0100

    Update t5e2premiocolores.py

--- ejercicios/t5e2premiocolores.py
+++ ejercicios/t5e2premiocolores.py
@@ -1,26 +1,21 @@
-#Pedimos a la usuario 5 veces que introduzca un color
+#Función pedir al usuario que introduzca un color
 def pedir_colores ():
     color = input ("Introduce un color: ")
     return color
 
 
-#Bucle solicitar 
+#Bucle solicitar 5 veces
 def solicitud_colores ():
-
     premio = "FELICIDADES ¡has conseguido un premio!"
     no_premio = "¡Prueba otra vez!"
 
-    for colores_usuaria in range (5):
+    for i in range (5):
         colores_usuaria = pedir_colores ()
 
-
         if colores_usuaria == "azul":
-             break
-        print (premio)
-        
-        print (no_premio)
-
-        colores_usuaria +=1
-
+             print (premio)
+             break  
+        else:
+            print (no_premio)
 
 solicitud_colores()
