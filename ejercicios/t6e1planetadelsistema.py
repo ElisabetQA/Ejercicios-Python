f74b8c44268a4297edb9413504cef249b36d23df
Elisabet Gacía <elisabet.gar@gmail.com>
2026-04-29 12:51:03 +0100

    Update t6e1planetadelsistema.py

--- ejercicios/t6e1planetadelsistema.py
+++ ejercicios/t6e1planetadelsistema.py
@@ -1,5 +1,5 @@
 #Lista de planetas, se añade en la lista el cero para pedir al usuario del 1 al 8
-planetas = ["Cero", "Mercurio", "Venus", "Tierra", "Marte", "Júpiter", "Saturno", "Urano", "Neptuno"]
+planetas = ["Mercurio", "Venus", "Tierra", "Marte", "Júpiter", "Saturno", "Urano", "Neptuno"]
 
 #Función pedir al usuario un número del 1 al 8
 def elige_numero ():
@@ -10,7 +10,7 @@ numero = elige_numero ()
 #Función mostrar número de la lista
 def mostrar_planeta (planetas,numero):
     if numero <= 8:
-        print (planetas [numero])
+        print (planetas [numero -1])
     else: 
         print ("El número introducido no está dentro de rango (1 al 8), vuelve a intentarlo. ")
 
